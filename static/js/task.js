37243bb88fade970fdda08f8a0fe3b10b4c01b77
tchamberlain <tchamberlain@uchicago.edu>
2023-02-27 17:12:14 -0500

    add stuff back

--- static/js/task.js
+++ static/js/task.js
@@ -75,40 +75,40 @@ mazeRewarded = _.shuffle(mazeRewarded);
 
 // timeline.push(continueInstructions(`<br><br><br><h1>Please review the consent form and prese continue to agree</h1><br><img src="static/images/consent1.png" width="425" height="550"><img src="static/images/consent2.png"  width="425" height="550"> <br><br>`))
 
-// // TESTING WM
-// timeline.push(wmInstructions())
-// timeline.push(wmPractice(wmPracticeSet))
-// //TESTING WM
+// TESTING WM
+//TESTING WM
 
 // // ENCODING
 // timeline.push(getProlificId(condition));
 // var scene = getOriginalScene();
 
 // // practice!!!!
-// timeline.push(mazeInstructions());
-// timeline.push(continueInstructions(`<br><br><br><h1>Click continue to begin!</h1><br>`, resetMazeVars));
-// timeline.push(practiceMaze(practiceItems1, rewarded=true));
-// timeline.push(continueInstructions(`<br><br><br><h1>Nice, you found a gold coin, and won a dollar bonus!</h1><br>`));
-// timeline.push(continueInstructions(`<br><br><br><h1>Now let's do a second practice maze!</h1><br>`, resetMazeVars));
-// timeline.push(practiceMaze(practiceItems2, rewarded=false));
-// timeline.push(continueInstructions(`<br><br><br><h1> In between each task, you will play a memory game <br><br> Maze 1 / 22 <br><br> </h1>`, resetMazeVars));
+timeline.push(mazeInstructions());
+timeline.push(continueInstructions(`<br><br><br><h1>Click continue to begin!</h1><br>`, resetMazeVars));
+timeline.push(practiceMaze(practiceItems1, rewarded=true));
+timeline.push(continueInstructions(`<br><br><br><h1>Nice, you found a gold coin, and won a dollar bonus!</h1><br>`));
+timeline.push(wmInstructions())
+timeline.push(wmPractice(wmPracticeSet))
+timeline.push(continueInstructions(`<br><br><br><h1>Now let's do a second practice maze!</h1><br>`, resetMazeVars));
+timeline.push(practiceMaze(practiceItems2, rewarded=false));
+timeline.push(continueInstructions(`<br><br><br><h1> In between each task, you will play a memory game <br><br> Maze 1 / 22 <br><br> </h1>`, resetMazeVars));
 
-// timeline.push(continueInstructions(`<br><br><br><h1> That's it for practice, now you will start the first maze! <br><br> Maze 1 / 22 <br><br> </h1>`, resetMazeVars));
+timeline.push(continueInstructions(`<br><br><br><h1> That's it for practice, now you will start the first maze! <br><br> Maze 1 / 22 <br><br> </h1>`, resetMazeVars));
 
-// breakLength = _.shuffle(breakLength);
-// wmDisplaySets = _.shuffle(wmDisplaySets);
-// var itemsIndex = 0;
-// _.each([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21], function (i) {
-//     // timeline.push(runMaze(i, itemsIndex));
-//     var mazeLength = mazeLengths[i];
-//     var items = mazeItems.slice(itemsIndex, itemsIndex + mazeLength)
-//     console.log(items)
-//     itemsIndex += mazeLength;
-//     timeline.push(practiceMaze(items, rewarded = mazeRewarded[i]));
-//     itemsIndex += mazeLengths[i];
-//     // timeline.push(wmTask(wmDisplaySets[i]))
-//     timeline.push(continueInstructions(`<br><br><br><h1> Now you will start the next maze! <br><br> Maze ${i + 2} / 22 <br><br> </h1>`, resetMazeVars));
-// });
+breakLength = _.shuffle(breakLength);
+wmDisplaySets = _.shuffle(wmDisplaySets);
+var itemsIndex = 0;
+_.each([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21], function (i) {
+    // timeline.push(runMaze(i, itemsIndex));
+    var mazeLength = mazeLengths[i];
+    var items = mazeItems.slice(itemsIndex, itemsIndex + mazeLength)
+    console.log(items)
+    itemsIndex += mazeLength;
+    timeline.push(practiceMaze(items, rewarded = mazeRewarded[i]));
+    itemsIndex += mazeLengths[i];
+    // timeline.push(wmTask(wmDisplaySets[i]))
+    timeline.push(continueInstructions(`<br><br><br><h1> Now you will start the next maze! <br><br> Maze ${i + 2} / 22 <br><br> </h1>`, resetMazeVars));
+});
 
 //  BREAK
 // participants watch a 5 min movie
@@ -123,7 +123,7 @@ var breakTrial = {
     },
     trial_ends_after_video: true
 };
-// timeline.push(breakTrial);
+timeline.push(breakTrial);
 
 var testInstruct = continueInstructions('<h2>Now you will do a memory test</h2>');
 var testInstruct = continueInstructions("<h2>We will show you an item. <br><br> If you remember seeing the item during the Maze Game, answer 'Old'.<br><br> If you don't remember seeing the item, press 'New'<br></h2>");
@@ -146,22 +146,9 @@ mazeItems = _.shuffle(mazeItems);
 // We randomly selected 60 objects that were correctly identified as old objects during the Phase 2: recognition memory test.
 // If a participant did not have enough hit trials(in Experiment 1, this applied to two participants in the 24 - hour condition),
 //  the balance of trials was filled with miss trials, and these trials were removed from subsequent analyses.
-// misses = _.shuffle(misses);
-// hits = _.shuffle(hits);
-// console.log('misses', misses)
-// console.log('hits', hits)
 
-// var spatialTestList = hits.slice(0, 90);
-// if (spatialTestList.length < 90) {
-//     const itemsNeeded = 90 - spatialTestList.length;
-//     spatialTestList += misses.slice(0, itemsNeeded);
-// }
-// console.log('spatialTestList', spatialTestList)
-
-// spatialTestList = _.shuffle(spatialTestList);
 var spatialTestList = null;
 timeline.push(continueInstructions('<h2>Now you will do another type of memory test</h2>', kickOffSpatialTest))
-// timeline.push(continueInstructions('<h2>Using the arrow keys, move the item to where you remember seeing it in the maze game <br> Press space to contin</h2>', kickOffSpatialTest))
 timeline.push(spatialTestItemLoop())
 
 timeline.push(getFeedback());
